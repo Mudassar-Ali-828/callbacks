01641740a2bc40fe913bdca894fb2d0829b6f4b0
Mudassar <mudassar678jsjs@gmail.com>
2024-08-31 13:05:23 +0500

    studying the callbacks

---
+++ app.js
@@ -0,0 +1,14 @@
+// nesting
+
+let age = 19;
+
+if (age >= 18){
+    console.log("senior");
+    if(age>=60){
+        console.log("senior")
+    }else{
+        console.log("middle");
+    }
+} else{
+        console.log("child");
+    }
